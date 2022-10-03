8c12a571be4761ae92a862df1be4f2e5b21688bf
Kateryna Ostrynska <kateriinag@gmail.com>
2022-10-03 20:41:26 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+let formData = { email: '', message: '' };
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+getDataBase();
+
+function onFormInput(e) {
+  const { name, value } = e.target;
+  formData[name] = value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (refs.textarea.textarea === '' || refs.input.value === '') {
+    alert('Please make sure all fields are filled in correctly');
+  } else {
+    e.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+    console.log(formData);
+  }
+}
+
+function getDataBase() {
+  const getData = localStorage.getItem('feedback-form-state');
+  const parsedData = JSON.parse(getData);
+  if (parsedData) {
+    refs.input.value = parsedData.input;
+    refs.textarea.value = parsedData.textarea;
+  }
+}
